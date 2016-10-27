3e6bbb7f4dd56c70e6853ef48f31e192a97079fa
wangke <cn_wk@hotmail.com>
2016-10-28 00:53:42 +0800

    fix insert.py

--- sort/insert.py
+++ sort/insert.py
@@ -6,11 +6,12 @@ def insert_sort(lst):
     for i in range(1, len(lst)):
         j = i -1
         tmp = lst[i]
-        while j >= 0 and lst[i] < lst[j]:
+        while j >= 0 and tmp < lst[j]:
             lst[j +1] = lst[j]
             j -=1
         lst[j +1] = tmp
     return lst
 
 if '__main__' == __name__:
-    print insert_sort([1, 4, 2, 5, 10])
+    print insert_sort([1, 4, 5, 10, 2])
+    print insert_sort([4, 2, 1, 5, 10])
