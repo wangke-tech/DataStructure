571f7e17f171ae097a52df672bf9af1ef9d15e74
wangke <cn_wk@hotmail.com>
2016-10-28 23:35:53 +0800

    steps

---
+++ steps.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+动态规划问题 1:
+
+有n级台阶，一个人每次上1级或2级，问有多少种走完n级台阶的方法？
+"""
+
+
+def f(n):
+    if 0 < n <=2:
+        return n
+    else:
+        return f(n -1) + f(n -2)
+
+print __doc__
+
+n =int(raw_input('input a num:\n'))
+
+print '\nresult:\n', f(n)
